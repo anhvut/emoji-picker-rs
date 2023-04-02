2887708d3a0afddd7257dbc05535931eff0d8cf7
Anh-Vu Tran <anhvutran1@gmail.com>
2023-04-02 23:39:52 +0200

    basic search

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,37 @@
+use std::iter::{Iterator, zip};
 use std::rc::Rc;
 use emojis;
+use emojis::Emoji;
 use gtk::{Application, ApplicationWindow, Clipboard, EventBox, FlowBox, Label, Orientation, Paned, ScrolledWindow, SearchEntry};
 use gtk::gdk;
 use gtk::prelude::*;
+
+#[derive(Debug)]
+struct EmojiDescriptor {
+    pub emoji: &'static Emoji,
+    pub keywords: String,
+}
+
+impl EmojiDescriptor {
+    fn new(emoji: &'static Emoji, keywords: String) -> EmojiDescriptor {
+        EmojiDescriptor {
+            emoji,
+            keywords,
+        }
+    }
+}
+
+fn build_descriptor() -> Vec<EmojiDescriptor> {
+    let mut result = vec![];
+    for emoji in emojis::iter() {
+        let emoji_name = emoji.name();
+        let aliases = emoji.shortcodes().collect::<Vec<_>>().join(" ");
+        let keywords = format!("{emoji_name} {aliases}").to_lowercase();
+        result.push(EmojiDescriptor::new(emoji, keywords));
+    }
+    result
+}
+
 
 fn main() {
     let app = Application::new(
@@ -10,32 +39,36 @@ fn main() {
         gtk::gio::ApplicationFlags::empty(),
     );
 
-    app.connect_activate(|app| {
+    let emojis_descriptor_rc = Rc::new(build_descriptor());
+
+    app.connect_activate(move |app| {
         let window = ApplicationWindow::new(app);
         window.set_title("🚀 Emoji Picker");
         window.set_default_size(640, 480);
 
         let search_entry = SearchEntry::new();
-        let feedback_label = Rc::new(Label::new(None));
+        let feedback_label_rc = Rc::new(Label::new(None));
 
         let upper_content_paned = Paned::new(Orientation::Horizontal);
         upper_content_paned.pack1(&search_entry, true, false);
-        upper_content_paned.pack2(feedback_label.as_ref(), true, false);
+        upper_content_paned.pack2(feedback_label_rc.as_ref(), true, false);
 
         let main_horizontal_paned = Paned::new(Orientation::Horizontal);
 
         let left_content = Label::new(None);
 
         let right_window = ScrolledWindow::new(None::<&gtk::Adjustment>, None::<&gtk::Adjustment>);
-        let flow_box = FlowBox::new();
+        let flow_box_rc = Rc::new(FlowBox::new());
+        let flow_box = flow_box_rc.as_ref();
         right_window.set_policy(gtk::PolicyType::Automatic, gtk::PolicyType::Automatic);
 
-        for emoji in emojis::iter() {
+        for ed in emojis_descriptor_rc.as_ref() {
+            let emoji = ed.emoji;
             let emoji_str = emoji.as_str();
             let emoji_label = Label::new(None);
             emoji_label.set_markup(&format!("<span font=\"emoji 24\">{emoji_str}</span>"));
             let event_box = EventBox::new();
-            let feedback_label_context = feedback_label.clone();
+            let feedback_label_context = feedback_label_rc.clone();
             event_box.add(&emoji_label);
             event_box.connect_button_press_event(move |_, _| {
                 Clipboard::get(&gdk::SELECTION_CLIPBOARD).set_text(emoji_str);
@@ -45,7 +78,21 @@ fn main() {
             });
             flow_box.add(&event_box);
         }
-        right_window.add(&flow_box);
+        right_window.add(flow_box);
+
+        let flow_box_context = flow_box_rc.clone();
+        let emojis_descriptor_context = emojis_descriptor_rc.clone();
+        search_entry.connect_text_notify(move |f| {
+            let flow_box_ref = flow_box_context.as_ref();
+            let search_text = f.text().to_lowercase();
+            if search_text.is_empty() {
+                for child in &flow_box_ref.children() { child.show(); }
+                return;
+            }
+            for (ed, child) in zip(emojis_descriptor_context.as_ref(), &flow_box_ref.children()) {
+                if ed.keywords.contains(&search_text) { child.show(); } else { child.hide(); }
+            }
+        });
 
         main_horizontal_paned.add(&left_content);
         main_horizontal_paned.add(&right_window);
@@ -57,6 +104,5 @@ fn main() {
         window.add(&main_vertical_paned);
         window.show_all();
     });
-
     app.run();
 }
